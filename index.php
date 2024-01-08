76b0b84d26742b5ab406dabe42715fa5df1aeeb0
DoniBoye <donfokker@gmail.com>
2024-01-08 13:59:58 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+    $_SESSION;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <a href="logout.php">Logout</a>
+    <h1>This is the indexpage </h1>
+    <br>
+    Hello, Username.
+</body>
+</html>
