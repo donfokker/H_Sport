594587e3b33357989a99822791e12d0df6558860
DoniBoye <donfokker@gmail.com>
2024-01-15 14:11:48 +0100

    deleted the map script since we won't be using it in this project.

--- index.php
+++ index.php
@@ -283,7 +283,7 @@ session_start();
   </section>
 
   <!-- end fruits section -->
-  
+
   <!-- footer section -->
   <section class="container-fluid footer_section">
     <p>
@@ -295,34 +295,7 @@ session_start();
 
   <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
   <script type="text/javascript" src="js/bootstrap.js"></script>
-
-  <script>
-    // This example adds a marker to indicate the position of Bondi Beach in Sydney,
-    // Australia.
-    function initMap() {
-      var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 11,
-        center: {
-          lat: 40.645037,
-          lng: -73.880224
-        },
-      });
-
-      var image = 'images/maps-and-flags.png';
-      var beachMarker = new google.maps.Marker({
-        position: {
-          lat: 40.645037,
-          lng: -73.880224
-        },
-        map: map,
-        icon: image
-      });
-    }
-  </script>
-  <!-- google map js -->
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA8eaHt9Dh5H57Zh0xVTqxVdBFCvFMqFjQ&callback=initMap">
-  </script>
-  <!-- end google map js -->
+  
 </body>
 
 </html>
